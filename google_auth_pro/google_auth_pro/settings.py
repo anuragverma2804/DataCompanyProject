29592956883ac462f2da0065a55246a8fa3c13f8
Anurag Verma <59530077+anuragverma2804@users.noreply.github.com>
2022-05-31 23:01:38 +0530

    Stated template directory path

--- google_auth_pro/google_auth_pro/settings.py
+++ google_auth_pro/google_auth_pro/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'google_auth_pro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
